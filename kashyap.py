a7bcadaac5506661f997b04136f467a523b1c0e4
ADITYA KASHYAP <adityakashyapchs9931@gmail.com>
2026-07-15 00:08:12 +0530

    test 1

---
+++ kashyap.py
@@ -0,0 +1,12 @@
+a=int(input("enter the value"))
+match a:
+  case 1:
+    print("one")
+  case 2:
+    print("two")
+  case 3:
+    print("three")
+  case 4 :
+    print("four")
+  case _:
+    print("none")
